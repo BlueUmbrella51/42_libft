fa10891586d338dbf649e56599215fe34d9377c1
Laure Ravier <lravier@f1r4s16.codam.nl>
2019-04-01 08:16:49 +0200

    libft as first committed, with added functions

--- ft_lstremove.c
+++ ft_lstremove.c
@@ -1,34 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_realloc.c                                       :+:    :+:            */
+/*   ft_lstremove.c                                     :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: lravier <marvin@codam.nl>                    +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/03/31 10:24:11 by lravier       #+#    #+#                 */
-/*   Updated: 2019/03/31 14:04:40 by lravier       ########   odam.nl         */
+/*   Created: 2019/03/31 17:17:13 by lravier       #+#    #+#                 */
+/*   Updated: 2019/03/31 17:24:35 by lravier       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memrealloc(void *ptr, size_t size, size_t o_size)
+void	ft_lstremove(t_list **lst, t_list *elem, void (*del)(void *, size_t))
 {
-	void *nbuff;
+	t_list curr;
+	t_list next;
 
-	if (size == 0)
+	curr = *lst;
+	if (curr = elem)
 	{
-		free(ptr);
-		return (NULL);
+		ft_lstdel(lst, del);
 	}
-	if (!ptr)
-		return (ft_memalloc(size));
-	if (size <= o_size)
-		return (ptr);
-	nbuff = ft_memalloc(size);
-	if (!nbuff)
-		return (NULL);
-	ft_memcpy(nbuff, ptr, o_size);
-	free(ptr);
-	return (nbuff);
+	else if (!curr->next)
+		ft_lstdelone(	
 }
