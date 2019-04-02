0cdc7bfce150fa31ade2fa5b633a8a5271caa1e3
Laure Ravier <lravier@f0r2s15.codam.nl>
2019-04-02 17:03:33 +0200

    fixed

--- ft_memrealloc.c
+++ ft_memrealloc.c
@@ -6,31 +6,30 @@
 /*   By: lravier <marvin@codam.nl>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/31 10:24:11 by lravier       #+#    #+#                 */
-/*   Updated: 2019/04/01 15:26:32 by lravier       ########   odam.nl         */
+/*   Updated: 2019/04/02 17:03:04 by lravier       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-#include <stdio.h>
 void	*ft_memrealloc(void *ptr, size_t prev_size, size_t size)
 {
 	void *buff;
+	int mlen;
 
-	if (size == 0)
+	mlen = size;
+	if (size == 0 || !ptr)
 	{
+		if (ptr)
+			free(ptr);
 		return (ft_memalloc(size));
-		free(ptr);
 	}
-	if (!ptr)
-		return (ft_memalloc(size));
 	if (size <= prev_size)
-		return (ptr);
-	buff = ft_memalloc(size);
-	if (buff)
-	{
-		ft_memcpy(buff, ptr, prev_size);
-		free(ptr);
-	}
+		mlen = prev_size;
+	buff = ft_memalloc(mlen);
+	if (!buff)
+		return (NULL);
+	ft_memcpy(buff, ptr, prev_size);
+	free(ptr);
 	return (buff);
 }
